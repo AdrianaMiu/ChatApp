779837377ead83bfd2b94c923e08c962ffa771fa
Miu Adriana <adrianamiu55@yahoo.ro>
2022-11-15 16:27:13 +0200

    Adding get and post functions in APIView

--- profile-rest-api/profile_api/views.py
+++ profile-rest-api/profile_api/views.py
@@ -1,3 +1,37 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
+
+from profile_api import serializers
+
+
+class HelloApiView(APIView):
+    """Test API VIew"""
+    serializer_class = serializers.HelloSerializer
+
+    def get(self, request, format=None):
+        """Return a list of APIView features"""
+        an_apiview = [
+            'Uses HTTP method as function (get, post, patch, put, delete)',
+            'Is similar to a traditional Django View',
+            'Gives you the most control over you applocation logic',
+            'Is mapped manually to URLs',
+        ]
+
+        return Response({'message': 'Hello!', 'an_apiview': an_apiview})
+
+    def post(self, request):
+        """Create an hello message with our name"""
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid(): #daca inputul este valid
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}'
+            return Response({'message': message})
+        else:
+            return Response(
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST) #for user when tries to submmit an invalid input
 # Create your views here.
